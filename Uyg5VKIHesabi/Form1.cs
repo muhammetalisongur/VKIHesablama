94672d8609c5332f48202ce2a3f09601bc7cb170
Muhammet Ali Songur <muhammetalisongur@gmail.com>
2022-12-06 19:41:47 +0300

    Add project files.

---
+++ Uyg5VKIHesabi/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uyg5VKIHesabi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int agirlik = (int)numericUpDown1.Value;
+            double boy = Convert.ToDouble(textBox1.Text) / 100;
+
+            int vki = (int)(agirlik / (boy * boy));
+            label4.Text = "VKI: " + vki.ToString();
+
+            int altsinir;
+            int ustsinir;
+
+            if(radioButton1.Checked)
+            {
+                altsinir = 20;
+                ustsinir = 25;
+            }
+            else
+            {
+                altsinir = 19;
+                ustsinir = 24;
+            }
+            if (vki < altsinir)
+            {
+                label5.Text = "VKI dusuk, kilo almalisiniz";
+            }
+            else if (vki > ustsinir)
+            {
+                label5.Text = "VKI yuksek, kilo vermelisiniz";
+            }
+            else
+                label5.Text = "Ideal VKI";
+
+        }
+    }
+}
